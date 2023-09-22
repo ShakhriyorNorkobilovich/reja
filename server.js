3386447c635c6fe87bfbe15b0eed3b6106d8ccf8
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2023-09-22 23:31:35 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web serverni boshlash");
 const express = require ("express");
+const res = require ("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    }else{
+        user = JSON.parse(data)
+    }
+})
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -12,14 +23,21 @@ app.use(express.urlencoded({extended: true}));
 
 // 3: View code
 app.set("views", "views");
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1>HELLO WORLD by Khan</h1>`);
+app.post("/create-item", (req, res) => {
+   // console.log(req.body);
+   // res.json({ test: "success" });
+   //TODO: code with db here
 });
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovgalar bulimidadiz</h1>`);
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);
